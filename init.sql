fea6e88f5eff3c8e6ffbb0e8bce1d7b0b0b3b28a
mmdv3 <michal.dymowski99@gmail.com>
2021-06-13 22:48:06 +0200

    listy_city function

--- init.sql
+++ init.sql
@@ -44,38 +44,38 @@ end;
 $$
 language plpgsql;
 
---select flight(
---  'HEA', 'KBL',
---  '2011-01-01 00:00:00+03'::timestamptz,
---  '2011-01-01 00:00:01+03'::timestamptz);
---select flight(
---  'KBL', 'TIA',
---  '2011-01-01 00:00:00+03'::timestamptz,
---  '2011-01-01 00:00:01+03'::timestamptz);
---select flight(
---  'TIA', 'TEE',
---  '2011-01-01 00:00:00+03'::timestamptz,
---  '2011-01-01 00:00:01+03'::timestamptz);
---select flight(
---  'TEE', 'HEA',
---  '2011-01-01 00:00:00+03'::timestamptz,
---  '2011-01-01 00:00:01+03'::timestamptz);
---select flight(
---  'HEA', 'TIA',
---  '2011-01-01 00:00:00+03'::timestamptz,
---  '2011-01-01 00:00:01+03'::timestamptz);
---select flight(
---  'KBL', 'TEE',
---  '2011-01-01 00:00:00+03'::timestamptz,
---  '2011-01-01 00:00:01+03'::timestamptz);
---select flight(
---  'BJA', 'TMR',
---  '2011-01-01 00:00:00+03'::timestamptz,
---  '2011-01-01 00:00:01+03'::timestamptz,
---  'CZL',
---  '2011-01-01 00:00:02+03'::timestamptz,
---  '2011-01-01 00:00:03+03'::timestamptz
---  );
+select flight(
+  'HEA', 'KBL',
+  '2011-01-01 00:00:00+03'::timestamptz,
+  '2011-01-01 00:00:01+03'::timestamptz);
+select flight(
+  'KBL', 'TIA',
+  '2011-01-01 00:00:00+03'::timestamptz,
+  '2011-01-01 00:00:01+03'::timestamptz);
+select flight(
+  'TIA', 'TEE',
+  '2011-01-01 00:00:00+03'::timestamptz,
+  '2011-01-01 00:00:01+03'::timestamptz);
+select flight(
+  'TEE', 'HEA',
+  '2011-01-01 00:00:00+03'::timestamptz,
+  '2011-01-01 00:00:01+03'::timestamptz);
+select flight(
+  'HEA', 'TIA',
+  '2011-01-01 00:01:00+03'::timestamptz,
+  '2011-01-01 00:02:01+03'::timestamptz);
+select flight(
+  'KBL', 'TEE',
+  '2011-01-01 00:00:00+03'::timestamptz,
+  '2011-01-01 00:00:01+03'::timestamptz);
+select flight(
+  'BJA', 'TMR',
+  '2011-01-01 00:00:00+03'::timestamptz,
+  '2011-01-01 00:00:01+03'::timestamptz,
+  'CZL',
+  '2011-01-01 00:00:02+03'::timestamptz,
+  '2011-01-01 00:00:03+03'::timestamptz
+  );
 
 
 create or replace function list_flights_segment (
@@ -83,7 +83,8 @@ create or replace function list_flights_segment (
 returns table (
   segment_id int, flight_id int, takeoff_location varchar(3),
   takeoff_time timestamp with time zone , landing_location varchar(3),
-  landing_time timestamp with time zone)
+  landing_time timestamp with time zone,
+  distance int)
 as $$
 --declare 
 --  new_flight_id int;
@@ -99,8 +100,15 @@ begin
  -- join airport a_l
  -- on segment.landing_location like a_l.iatacode
 	
-  create temp table segments_with_distance on commit drop as
-  (with recursive cmp_segment_attr as (
+--  create temp table segments_with_distance on commit drop as
+  return query
+
+  select segments_with_distance.segment_id, segments_with_distance.flight_id,
+    segments_with_distance.takeoff_location, segments_with_distance.takeoff_time,
+   	segments_with_distance.landing_location, segments_with_distance.landing_time,
+	segments_with_distance.distance
+   	from (
+  with cmp_segment_attr as (
    	select segment.segment_id, segment.flight_id, segment.takeoff_location, a_t.latitude as t_latitude, a_t.longitude as t_longitude,
   		segment.landing_location , a_l.latitude as l_latitude, a_l.longitude as l_longitude
 	from segment
@@ -117,20 +125,17 @@ select segment.segment_id, segment.flight_id, segment.takeoff_location, segment.
 		 a_l.longitude|| ' ' || a_l.latitude ||')')::geography,
 	 	 ('LINESTRING(' || (select t_longitude from cmp_segment_attr)
 		   	||' '|| (select t_latitude from cmp_segment_attr)||', '||
-		   (select l_longitude from cmp_segment_attr)||' '|| (select l_latitude from cmp_segment_attr)|| ')')::geography))::float
-   	as does_intersect 
+		   (select l_longitude from cmp_segment_attr)||' '|| (select l_latitude from cmp_segment_attr)|| ')')::geography))::int
+   	as distance 
   from segment
   join airport a_t
   on segment.takeoff_location like a_t.iatacode
   join airport a_l
-  on segment.landing_location like a_l.iatacode);
+  on segment.landing_location like a_l.iatacode
+	) segments_with_distance
 
-  return query
-  select segments_with_distance.segment_id, segments_with_distance.flight_id,
-    segments_with_distance.takeoff_location, segments_with_distance.takeoff_time,
-   	segments_with_distance.landing_location, segments_with_distance.landing_time from segments_with_distance
-  where segments_with_distance.segment_id != cmp_segment_id
-  and does_intersect < 1 --comparing floats with zero is risky
+  --where segments_with_distance.segment_id != cmp_segment_id
+  --and does_intersect < 1 --comparing floats with zero is risky
 ;
 end;
 $$
@@ -141,15 +146,21 @@ create or replace function list_flight (cmp_flight_id int)
 returns table (
   segment_id int, flight_id int, takeoff_location varchar(3),
   takeoff_time timestamp with time zone , landing_location varchar(3),
-  landing_time timestamp with time zone)
+  landing_time timestamp with time zone,
+  distance int)
 as $$
 begin
+  return query
   with ids as (
 	select segment.segment_id from segment
 	where segment.flight_id = cmp_flight_id)
   select l.*
   from ids,
-  lateral list_flights_segment(ids.segment_id) l;
+  lateral list_flights_segment(ids.segment_id) l
+  --where segments_with_distance.segment_id != cmp_segment_id
+  where l.segment_id not in (select ids.segment_id from ids)
+  and l.distance < 1 --comparing floats with zero is risky
+  ;
 end;
 $$ language plpgsql;
 
@@ -200,8 +211,74 @@ $$ language plpgsql;
 --  --limit 2
 --;
 
+create or replace function list_city_segment(takeoff_longitude float, takeoff_latitude float, landing_longitude float, 
+  landing_latitude float)
+returns table (name varchar, prov varchar, country varchar, distance float) as $$
+begin
+  return query
+  select city.name, city.province, city.country, 
+  (st_distance( ('LINESTRING(' || takeoff_longitude || ' ' || takeoff_latitude || ', ' || landing_longitude || ' ' 
+		  || landing_latitude || ')')::geography, ('POINT(' || city.longitude || ' ' || city.latitude || ')')::geography) )::float
+  from city;
+  --limit 10; --temporary , for test
+end;
+$$ language plpgsql;
+--, distance float) as $$
+-- , min(l.distance)
+create or replace function list_city(arg_flight_id int, dist numeric)
+returns table (name varchar, prov varchar, country varchar) as $$ 
+begin
+  return query
+  with flight_segment as (
+	select a1.longitude as takeoff_longitude, a1.latitude as takeoff_latitude, 
+	a2.longitude as landing_longitude, a2.latitude as landing_latitude	
+	from segment
+	join airport a1
+	on segment.takeoff_location like a1.iatacode
+	join airport a2
+	on segment.landing_location like a2.iatacode
+	where segment.flight_id = arg_flight_id)
+  select l.name, l.prov, l.country
+  from flight_segment,
+  lateral list_city_segment(
+	flight_segment.takeoff_longitude,
+	flight_segment.takeoff_latitude,
+	flight_segment.landing_longitude,
+	flight_segment.landing_latitude) l
+  group by l.name, l.prov, l.country
+  having min(l.distance) < dist * 1000
+  order by l.name asc;
+end;
+$$ language plpgsql;
 
 
+--SELECT ST_Distance('LINESTRING(-122.33 47.606, 0.0 51.5)'::geography, 'POINT(-21.96 64.15)'::geography)/1000 as distance;
+
+
+
+create or replace function list_airport (airport_iatacode varchar(3), -- add limits (n)
+  n int) returns table (flight_id int) as $$
+begin
+  return query
+  select segment.flight_id
+  from segment
+  where segment.takeoff_location like airport_iatacode 
+  order by segment.takeoff_time DESC, segment.flight_id
+  limit n;
+end;
+$$ language plpgsql;
+
+---- debug version
+--create or replace function list_airport (airport_iatacode varchar(3), -- add limits (n)
+--  n int) returns table (flight_id int, takeoff_time timestamptz) as $$
+--begin
+--  return query
+--  select segment.flight_id, segment.takeoff_time
+--  from segment
+--  where segment.takeoff_location like airport_iatacode 
+--  order by segment.takeoff_time DESC;
+--end;
+--$$ language plpgsql;
 
 
 
